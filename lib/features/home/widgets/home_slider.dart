894a106b9cb1fc1d79c455f05f0fc48683c0ee4d
berkaysaahin <berkaynihas@gmail.com>
2024-03-17 19:12:03 +0300

    home slider

--- lib/features/home/widgets/home_slider.dart
+++ lib/features/home/widgets/home_slider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foresight_news_and_articles/dummy.dart';
+import 'package:foresight_news_and_articles/features/home/widgets/home_slider_indicator_item.dart';
 import 'package:foresight_news_and_articles/features/home/widgets/home_slider_item.dart';
 
 class HomeSlider extends StatefulWidget {
@@ -11,8 +12,15 @@ class HomeSlider extends StatefulWidget {
 
 class _HomeSliderState extends State<HomeSlider> {
   late final PageController _pageController;
+  late final ScrollController _scrollController;
+  late final double _indicatorsVisibleWidth;
 
   int _pageIndex = 0;
+
+  final _displayIndicatorsCount = 5.0;
+  final _indicatorWidth = 8.0;
+  final _activeIndicatorWidth = 32.0;
+  final _indicatorMargin = const EdgeInsets.symmetric(horizontal: 1);
 
   @override
   void initState() {
@@ -21,6 +29,10 @@ class _HomeSliderState extends State<HomeSlider> {
       viewportFraction: 0.8,
       initialPage: 1000,
     );
+    _scrollController = ScrollController(
+      initialScrollOffset: _calculateIndicatorOffset(),
+    );
+    _indicatorsVisibleWidth = _calculateIndicatorWidth();
   }
 
   @override
@@ -29,28 +41,67 @@ class _HomeSliderState extends State<HomeSlider> {
     super.dispose();
   }
 
+  double _calculateIndicatorWidth() {
+    final indicatorTotalWidth =
+        _indicatorWidth + _indicatorMargin.left + _indicatorMargin.right;
+    final activeIndicatorTotalWidth =
+        _activeIndicatorWidth + _indicatorMargin.left + _indicatorMargin.right;
+    return activeIndicatorTotalWidth +
+        (_displayIndicatorsCount - 1) * indicatorTotalWidth;
+  }
+
+  double _calculateIndicatorOffset() {
+    final indicatorsCountBeforeCentral = (_displayIndicatorsCount - 1) / 2;
+    final offset =
+        (_indicatorWidth + _indicatorMargin.left + _indicatorMargin.right) *
+            (_pageIndex + 999 - indicatorsCountBeforeCentral);
+    return offset;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        height: 235,
-        child: PageView.builder(
-          onPageChanged: (value) {
-            setState(() {
-              _pageIndex = value % newsrItems.length;
-            });
-          },
-          controller: _pageController,
-          itemBuilder: (context, index) {
-            final i = index % newsrItems.length;
-            return HomeSliderItem(
-              isActive: _pageIndex == i,
-              imageAssetPath: newsrItems[i]['imageAssetPath']!,
-              category: newsrItems[i]['category']!,
-              title: newsrItems[i]['title']!,
-              author: newsrItems[i]['author']!,
-              date: DateTime.parse(newsrItems[i]['date']!),
-            );
-          },
-        ));
+    return Column(
+      children: [
+        SizedBox(
+            height: 235,
+            child: PageView.builder(
+              onPageChanged: (value) {
+                setState(() {
+                  _pageIndex = value % newsrItems.length;
+                });
+              },
+              controller: _pageController,
+              itemBuilder: (context, index) {
+                final i = index % newsrItems.length;
+                return HomeSliderItem(
+                  isActive: _pageIndex == i,
+                  imageAssetPath: newsrItems[i]['imageAssetPath']!,
+                  category: newsrItems[i]['category']!,
+                  title: newsrItems[i]['title']!,
+                  author: newsrItems[i]['author']!,
+                  date: DateTime.parse(newsrItems[i]['date']!),
+                );
+              },
+            )),
+        const SizedBox(
+          height: 10,
+        ),
+        SizedBox(
+          width: _indicatorsVisibleWidth,
+          height: _indicatorWidth,
+          child: ListView.builder(
+              controller: _scrollController,
+              itemBuilder: (context, index) {
+                final i = index % newsrItems.length;
+                return HomeSliderIndicatorItem(
+                  isActive: _pageIndex == i,
+                  activeWidth: _activeIndicatorWidth,
+                  width: _indicatorWidth,
+                  margin: _indicatorMargin,
+                );
+              }),
+        )
+      ],
+    );
   }
 }
---
+++ lib/features/home/widgets/home_slider_indicator_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:foresight_news_and_articles/theme/app_colors.dart';
+
+class HomeSliderIndicatorItem extends StatelessWidget {
+  final bool isActive;
+  final double activeWidth;
+  final double width;
+  final EdgeInsets margin;
+
+  const HomeSliderIndicatorItem(
+      {Key? key,
+      required this.isActive,
+      required this.activeWidth,
+      required this.width,
+      required this.margin})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      margin: margin,
+      width: isActive ? activeWidth : width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(width),
+        color: isActive ? Theme.of(context).primaryColor : AppColors.osloGray,
+      ),
+    );
+  }
+}
+
+//5.23
